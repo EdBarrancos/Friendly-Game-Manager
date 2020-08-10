9e401947ae1b48321768d2587584df14f6b969ce
EdBarrancos <eduardobarrancos11488@gmail.com>
2020-08-10 12:38:11 +0100

    First and Final Commit

---
+++ strdup/strdup.c
@@ -0,0 +1,22 @@
+/* 
+ * File: strdup.c
+ * Author: Eduardo Barrancos
+ * Description: Alloc memory for strings
+*/
+#include "strdup.h"
+
+/* Allocs memory for a string. Returns a pointer to that memory */
+char *my_strdup(char *source){
+    char *new;
+    char *_new;
+    int len = strlen(source) + 1;
+
+    new = malloc(len + 1);
+    _new = new;
+
+    while (*source)
+        *_new++ = *source++;
+        
+    *_new = '\0';
+    return new;
+}
---
+++ strdup/strdup.h
@@ -0,0 +1,16 @@
+/* 
+ * File: strdup.h
+ * Author: Eduardo Barrancos
+ * Description: Alloc memory for strings
+*/
+#ifndef _STRDUP_
+#define _STRDUP_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Allocs memory for a string. Returns a pointer to that memory */
+char *my_strdup(char *source);
+
+#endif
